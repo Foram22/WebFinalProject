50ccd15f30ee59018eaa19edafacf7a4be33efbc
Foram22 <foramjpatel5778@gmail.com>
2023-08-04 18:07:40 -0400

    First commit

---
+++ WebFinalProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebFinalProject.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
