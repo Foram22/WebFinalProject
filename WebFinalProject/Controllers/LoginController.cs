8cc820edf9e30be38c5e27e2d7f2d67c9f168bb6
Foram22 <foramjpatel5778@gmail.com>
2023-08-09 16:30:06 -0400

    Login page created and design is done. and also two new pages added to the application register page and forgot password page.

---
+++ WebFinalProject/Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebFinalProject.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: /<controller>/
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        { 
+            if (username == "yourusername" && password == "yourpassword")
+            {
+                // Authentication successful, redirect to a dashboard or other page
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                // Authentication failed, show an error message
+                ViewBag.ErrorMessage = "Invalid username or password";
+                return View();
+            }
+        }
+    }
+}
+
