04356d9c97a8e57ddc8a29cf009fde86f81da8c4
Foram22 <foramjpatel5778@gmail.com>
2023-08-11 18:38:38 -0400

    login successfully done with firebase sign in user eith email and password.

--- WebFinalProject/Controllers/LoginController.cs
+++ WebFinalProject/Controllers/LoginController.cs
@@ -2,8 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using Firebase.Auth;
+using Firebase.Database;
+using Firebase.Database.Query;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using WebFinalProject.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,33 +16,51 @@ namespace WebFinalProject.Controllers
 {
     public class LoginController : Controller
     {
-
+        FirebaseAuthProvider auth;
         public LoginController()
         {
-            
+            auth = new FirebaseAuthProvider(new FirebaseConfig("AIzaSyCGXTsFk3C8W5k8pA2rLmbv6jag8CJ95_8"));
         }
 
 
-    // GET: /<controller>/
-    public ActionResult Login()
+        // GET: /<controller>/
+        public ActionResult Login()
         {
             return View();
         }
 
         [HttpPost]
-        public ActionResult Login(string email, string password)
-        { 
-            //if (email == "foram@gmail.com" && password == "Foram@2211")
-            //{
-                // Authentication successful, redirect to a dashboard or other page
-                return RedirectToAction("Home", "Dashboard");
-            //}
-            //else
-            //{
-            //    // Authentication failed, show an error message
-            //    ViewBag.ErrorMessage = "Invalid username or password";
-            //    return View();
-            //}
+        public async Task<IActionResult> Login(string email, string password)
+        {
+            try
+            {
+                //create the user
+                var authResult = await auth.SignInWithEmailAndPasswordAsync(email, password);
+
+                if (authResult != null && authResult.User != null)
+                {
+                    var userId = authResult.User.LocalId;
+
+                    var firebaseClient = new FirebaseClient("https://facultymeets-default-rtdb.firebaseio.com/");
+                    var users = firebaseClient.Child("users");
+                    var currentUser = users.Child(userId);
+
+                    TempData["User"] = JsonConvert.SerializeObject(currentUser);
+                    return RedirectToAction("Home", "Dashboard");
+                }
+                else {
+                    ViewBag.ErrorMessage = "Email ID does not exist. Please register email id first and then login again.";
+                }
+
+                return View("Login");
+            }
+            catch (FirebaseAuthException ex)
+            {
+                ModelState.AddModelError(String.Empty, ex.Message);
+                ViewBag.ErrorMessage = ex.Message;
+                return View("Login");
+            }
+
         }
     }
 }
