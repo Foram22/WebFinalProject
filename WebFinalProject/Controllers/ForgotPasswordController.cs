9b8035317373293490f54bbd5d25dc117a8a47d3
Foram22 <foramjpatel5778@gmail.com>
2023-08-17 14:32:29 -0400

    revert changes.

--- WebFinalProject/Controllers/ForgotPasswordController.cs
+++ WebFinalProject/Controllers/ForgotPasswordController.cs
@@ -2,11 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Firebase.Auth;
-using Firebase.Database;
-using Firebase.Database.Query;
 using Microsoft.AspNetCore.Mvc;
-using WebFinalProject.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -21,57 +17,18 @@ namespace WebFinalProject.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult> ForgotAsync(string email, string password, string confirm_password)
+        public ActionResult Forgot(string email, string password, string confirm_password)
         {
-            if (password == confirm_password)
+            if (email != null && password != null && password == confirm_password)
             {
-                try
-                {
-                    var firebaseClient = new FirebaseClient("https://facultymeets-default-rtdb.firebaseio.com/");
-                    var userRef = await firebaseClient.Child("users").OrderBy("Email").EqualTo(email).OnceAsync<UserModel>();
-
-                    if (userRef.Any())
-                    {
-                        var user = userRef.First().Object;
-                        user.Password = password;
-
-                        if (user.Role == "Faculty" || user.Role == "faculty")
-                        {
-                            await firebaseClient.Child("faculty").Child(user.Id).PutAsync(user);
-                        }
-                        await firebaseClient.Child("users").Child(user.Id).PutAsync(user);
-
-                        return RedirectToAction("Login", "Login");
-                    }
-                    else
-                    {
-                        ViewBag.ErrorMessage = "Email not found.";
-                        return View("Forgot");
-                    }
-                }
-                catch (FirebaseAuthException ex)
-                {
-                    ModelState.AddModelError(String.Empty, ex.Message);
-                    ViewBag.ErrorMessage = ex.Message;
-                    return View("Forgot");
-                }
+                return RedirectToAction("Login", "Login");
             }
             else
             {
-                ViewBag.ErrorMessage = "Password mismatch";
-                return View("Forgot");
+                // Authentication failed, show an error message
+                ViewBag.ErrorMessage = "Invalid username or password";
+                return View();
             }
-
-            //if (email != null && password != null && password == confirm_password)
-            //{
-            //    return RedirectToAction("Login", "Login");
-            //}
-            //else
-            //{
-            //    // Authentication failed, show an error message
-            //    ViewBag.ErrorMessage = "Invalid username or password";
-            //    return View();
-            //}
         }
     }
 }
